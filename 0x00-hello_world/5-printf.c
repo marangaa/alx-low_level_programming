5fb7c2356f095d5ed043862c424c83655dc96bf6
maranga <rchdmaranga@gmail.com>
2022-06-03 00:35:05 +0300

    5 file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 /**
- * function goes here
- * returns 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
+	/* int is the return type for the function */
 {
+	/* prints the string */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
