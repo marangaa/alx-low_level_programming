f45719a99d0f816d01b2577528b946dd85f20316
maranga <rchdmaranga@gmail.com>
2022-06-03 00:00:23 +0300

    fix chnges

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - Entry point
+ * this function prints out the string
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n);
