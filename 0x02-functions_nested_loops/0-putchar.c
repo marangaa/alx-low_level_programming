094b4427a4e5c655b2630195877aed8e0f785a36
maranga <rchdmaranga@gmail.com>
2022-06-07 19:56:30 +0300

    task file for task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,16 +7,15 @@
  */
 int main(void)
 {
-	letter[9] = "_putchar";
+	char[9] = "_putchar";
 
 	int i = 0;
 
 	for (i = 0; i < 8; i++)
 	{
-		_putchar(letter[i])
+		_putchar(letter[i]);
 	}
 	_putchar("\n");
-
 
 	return (0);
 }
