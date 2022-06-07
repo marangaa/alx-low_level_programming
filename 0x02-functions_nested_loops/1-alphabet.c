8640ea45df048ddc6f9b8f571cb95e48d749cd2a
maranga <rchdmaranga@gmail.com>
2022-06-07 20:28:01 +0300

    task file 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lower case
+ *
+ *
+ */
+
+void main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
