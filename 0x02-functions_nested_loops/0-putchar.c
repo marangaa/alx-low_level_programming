3b2fc9eb16c23ef6bc731519452c7d6c72d9db37
maranga <rchdmaranga@gmail.com>
2022-06-07 19:21:48 +0300

    files for 0x02

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - prints "_putchar"
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	_putchar("_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+readme file for this project folder, learning about C functions
+- custom C functions
+- function prototypes
