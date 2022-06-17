28b711ceeb5a9c9bf893ed06de013f29cc066489
maranga <rchdmaranga@gmail.com>
2022-06-17 11:30:30 +0300

    file for task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,23 +5,21 @@
  * @s: An input string to encode using rot13
  * Return: An encode string
  */
+
 char *rot13(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+        /* You do this until you find a '\0' */
+        for( i = 0; s[i] != '\0' ; i++ )
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
-		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
-		}
-		i++;
-	}
+		/* Use the pointer notation if you passed a pointer. */
+		/* If the letter is between a and m you can simply  sum it. */
+		if( *(s + i ) >= 'a' && *(s + i ) < 'n')
+			*(s + i ) += 13;
+		/* If the letter is between the n and z you have to do the opposite.*/
+		else if(*(s + i ) >= 'n' && *(s + i ) <= 'z')
+			*(s + i ) -= 13;
+        }
 	return (s);
 }
